2165f2066b76dac657d1d22694672d585c2a3ef1
David Torres <davids182009@gmail.com>
2016-07-01 15:33:48 -0500

    Se agrega la aplicación con funcionalidades como  de ubicación de usuario, se adiciona el panel inferior para interactuar, se adapta el mapa para que se muestre en 2D, se agregaron Toast a todos los botones de la aplicación.

--- app/src/main/java/com/ambient/testmenu/MainActivity.java
+++ app/src/main/java/com/ambient/testmenu/MainActivity.java
@@ -1,8 +1,14 @@
 package com.ambient.testmenu;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Location;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.UiThread;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -12,31 +18,45 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
 
+import com.mapbox.mapboxsdk.camera.CameraPosition;
+import com.mapbox.mapboxsdk.geometry.LatLng;
+import com.mapbox.mapboxsdk.location.LocationListener;
+import com.mapbox.mapboxsdk.location.LocationServices;
 import com.mapbox.mapboxsdk.maps.MapView;
 import com.mapbox.mapboxsdk.maps.MapboxMap;
 import com.mapbox.mapboxsdk.maps.OnMapReadyCallback;
+
+import com.roughike.bottombar.BottomBar;
+import com.roughike.bottombar.OnMenuTabSelectedListener;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
 
     private MapView mapView;
+    private MapboxMap map;
+    LocationServices locationServices;
+    FloatingActionButton floatingActionButton;
+
+    private static final int PERMISSIONS_LOCATION = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+//        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+//        fab.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Snackbar.make(view, "Ejecutar la ubicacion de usuario...", Snackbar.LENGTH_LONG)
+//                        .setAction("Action", null).show();
+//            }
+//        });
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
@@ -44,6 +64,7 @@ public class MainActivity extends AppCompatActivity
         drawer.setDrawerListener(toggle);
         toggle.syncState();
 
+        //Visualizacion del mapa....
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
 
@@ -55,6 +76,59 @@ public class MainActivity extends AppCompatActivity
             @Override
             public void onMapReady(MapboxMap mapboxMap) {
                 // Customize map with markers, polylines, etc.
+                map = mapboxMap;
+            }
+        });
+
+
+ /*       //Barra inferior si se quiere poner con bottom!! Falta mirar como se hace :/ ....
+        BottomBar bottomBar = BottomBar.attach(this, savedInstanceState);
+        bottomBar.setItemsFromMenu(R.menu.menu_bottom, new OnMenuTabSelectedListener() {
+            public void onMenuItemSelected(int itemId) {
+                Context context = getApplicationContext();
+                int duration = Toast.LENGTH_SHORT;
+
+                switch (itemId) {
+                    case R.id.recent_item:
+                        //Snackbar.make(coordinatorLayout, "Recent Item Selected", Snackbar.LENGTH_LONG).show();
+                        Toast.makeText(context, "Presiono Items Recientes", duration).show();
+                        break;
+                    case R.id.favorite_item:
+                        //Snackbar.make(coordinatorLayout, "Favorite Item Selected", Snackbar.LENGTH_LONG).show();
+                        Toast.makeText(context, "Presiono Items Favoritos", duration).show();
+                        break;
+                    case R.id.location_item:
+                        //Snackbar.make(coordinatorLayout, "Location Item Selected", Snackbar.LENGTH_LONG).show();
+                        Toast.makeText(context, "Presiono Items Localizacion", duration).show();
+                        break;
+                }
+            }
+        });
+
+        // Set the color for the active tab. Ignored on mobile when there are more than three tabs.
+        bottomBar.setActiveTabColor("#C2185B");
+
+//        // Use the dark theme. Ignored on mobile when there are more than three tabs.
+//        bottomBar.useDarkTheme(true);
+//
+//        // Use custom text appearance in tab titles.
+//        bottomBar.setTextAppearance(R.style.);
+//
+//        // Use custom typeface that's located at the "/src/main/assets" directory. If using with
+//        // custom text appearance, set the text appearance first.
+//        bottomBar.setTypeFace("MyFont.ttf");*/
+
+
+        //Ubicacion del usuario por medio del boton flotante...
+        locationServices = LocationServices.getLocationServices(MainActivity.this);
+
+        floatingActionButton = (FloatingActionButton) findViewById(R.id.fab);
+        floatingActionButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (map != null) {
+                    toggleGps(!map.isMyLocationEnabled());
+                }
             }
         });
     }
@@ -84,31 +158,36 @@ public class MainActivity extends AppCompatActivity
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
+//        if (id == R.id.action_settings) {
+//            return true;
+//        }
 
         return super.onOptionsItemSelected(item);
     }
 
+    //Metodo para controlar los eventos del menu lateral....
     @SuppressWarnings("StatementWithEmptyBody")
     @Override
     public boolean onNavigationItemSelected(MenuItem item) {
+
+        Context context = getApplicationContext();
+        int duration = Toast.LENGTH_SHORT;
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
         if (id == R.id.nav_camera) {
             // Handle the camera action
+            Toast.makeText(context, "Presiono Boton Cosa 1...", duration).show();
         } else if (id == R.id.nav_gallery) {
-
+            Toast.makeText(context, "Presiono Boton Cosa 2...", duration).show();
         } else if (id == R.id.nav_slideshow) {
-
+            Toast.makeText(context, "Presiono Boton Cosa 3...", duration).show();
         } else if (id == R.id.nav_manage) {
-
+            Toast.makeText(context, "Presiono Boton Cosa 4...", duration).show();
         } else if (id == R.id.nav_share) {
-
+            Toast.makeText(context, "Presiono Boton Cosa 1 Configuracion...", duration).show();
         } else if (id == R.id.nav_send) {
-
+            Toast.makeText(context, "Presiono Boton Cosa 2 Configuracion...", duration).show();
         }
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
@@ -116,33 +195,90 @@ public class MainActivity extends AppCompatActivity
         return true;
     }
 
-//    @Override
-//    public void onResume() {
-//        super.onResume();
-//        mapView.onResume();
-//    }
-//
-//    @Override
-//    public void onPause() {
-//        super.onPause();
-//        mapView.onPause();
-//    }
-//
-//    @Override
-//    public void onSaveInstanceState(Bundle outState) {
-//        super.onSaveInstanceState(outState);
-//        mapView.onSaveInstanceState(outState);
-//    }
-//
-//    @Override
-//    public void onLowMemory() {
-//        super.onLowMemory();
-//        mapView.onLowMemory();
-//    }
-//
-//    @Override
-//    protected void onDestroy() {
-//        super.onDestroy();
-//        mapView.onDestroy();
-//    }
+    //Metodos para el ciclo de vida del activity..
+    @Override
+    public void onResume() {
+        super.onResume();
+        mapView.onResume();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        mapView.onPause();
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        mapView.onSaveInstanceState(outState);
+    }
+
+    @Override
+    public void onLowMemory() {
+        super.onLowMemory();
+        mapView.onLowMemory();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        mapView.onDestroy();
+    }
+    ////
+
+    //Metodo para conseguir la ubicacion del usuario...
+    @UiThread
+    public void toggleGps(boolean enableGps) {
+        if (enableGps) {
+            // Check if user has granted location permission
+            if (!locationServices.areLocationPermissionsGranted()) {
+                ActivityCompat.requestPermissions(this, new String[]{
+                        Manifest.permission.ACCESS_COARSE_LOCATION,
+                        Manifest.permission.ACCESS_FINE_LOCATION}, PERMISSIONS_LOCATION);
+            } else {
+                enableLocation(true);
+            }
+        } else {
+            enableLocation(false);
+        }
+    }
+
+    //Metodo encargado de setear la ubicacion del usuario...
+    private void enableLocation(boolean enabled) {
+        if (enabled) {
+            locationServices.addLocationListener(new LocationListener() {
+                @Override
+                public void onLocationChanged(Location location) {
+                    if (location != null) {
+                        // Move the map camera to where the user location is
+                        map.setCameraPosition(new CameraPosition.Builder()
+                                .target(new LatLng(location))
+                                .zoom(16)
+                                .build());
+                    }
+                }
+            });
+            //Setear iconos dependiendo del clic en el boton de ubicacion...
+            floatingActionButton.setImageResource(R.drawable.ic_localizacion_usuario_desac);
+        } else {
+            floatingActionButton.setImageResource(R.drawable.ic_localizacion_usuario);
+        }
+        // Enable or disable the location layer on the map
+        map.setMyLocationEnabled(enabled);
+    }
+
+    //Metodo encargado de revisar el permiso de ubicacion del dispositivo...
+    @Override
+    public void onRequestPermissionsResult(
+            int requestCode, @NonNull String permissions[], @NonNull int[] grantResults) {
+        switch (requestCode) {
+            case PERMISSIONS_LOCATION: {
+                if (grantResults.length > 0 &&
+                        grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    enableLocation(true);
+                }
+            }
+        }
+    }
 }
